6fd4764318191fe708866b2018660b3f5e671dc2
Steven Benmoha <steven.benmoha@gmail.com>
2017-11-12 15:47:53 -0500

    Added few things

---
+++ src/view/LoginController.java
@@ -0,0 +1,16 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+	
+	@FXML
+	Button quitButton, loginButton;
+	@FXML
+	TextField usernameTextField;
+	
+	
+
+}
