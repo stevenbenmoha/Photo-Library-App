bdae732373e2950f9636965f779cb2f6c0dd733d
Steven Benmoha <steven.benmoha@gmail.com>
2017-11-12 16:19:27 -0500

    Added AdminController

---
+++ src/app/LoginController.java
@@ -0,0 +1,29 @@
+package app;
+
+import java.util.ArrayList;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class LoginController {
+	
+	@FXML
+	Button quitButton, loginButton;
+	@FXML
+	TextField usernameTextField;
+	
+	private ObservableList<User> userList;
+	
+	
+	public void start(Stage primaryStage) {
+		
+				
+	}
+	
+	
+
+}
--- src/app/Photos.java
+++ src/app/Photos.java
@@ -11,6 +11,7 @@ import javafx.scene.*;
 
 
 
+
 public class Photos extends Application {
 	@Override
 	public void start(Stage primaryStage) {
